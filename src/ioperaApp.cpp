58247a8639bb3740104871a255f8a038642156de
Jens Wunderling <jens@sport4minus.de>
2011-02-10 16:27:22 +0100

    initial commit

---
+++ src/ioperaApp.cpp
@@ -0,0 +1,35 @@
+#include "cinder/app/AppBasic.h"
+#include "cinder/gl/gl.h"
+
+using namespace ci;
+using namespace ci::app;
+using namespace std;
+
+class ioperaApp : public AppBasic {
+  public:
+	void setup();
+	void mouseDown( MouseEvent event );	
+	void update();
+	void draw();
+};
+
+void ioperaApp::setup()
+{
+}
+
+void ioperaApp::mouseDown( MouseEvent event )
+{
+}
+
+void ioperaApp::update()
+{
+}
+
+void ioperaApp::draw()
+{
+	// clear out the window with black
+	gl::clear( Color( 0, 0, 0 ) ); 
+}
+
+
+CINDER_APP_BASIC( ioperaApp, RendererGl )
